d79c70579ecec374946b288d19276acb28e5ca07
Renan Silva <renansilvaytB@gmail.com>
2025-11-10 10:10:01 -0300

    feat: header and base layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,19 @@
+import { Scroll, Timer } from 'phosphor-react'
+import logo from '../assets/logo.svg'
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
     return (
-        <header></header>
+        <header className="flex items-center justify-between ">
+            <img src={logo} alt="Logo do site" />
+            <nav className='flex items-center gap-2'>
+                <NavLink title='Timer' to="/" className={({ isActive }) => `size-12 flex justify-center items-center text-gray-100 border-y-[3px] border-solid border-transparent hover:border-b-green-500 ${isActive ? 'text-green-500' : ''}`}>
+                    <Timer size={28} />
+                </NavLink>
+                <NavLink title='History' to="/history" className={({ isActive }) => `size-12 flex justify-center items-center text-gray-100 border-y-[3px] border-solid border-transparent hover:border-b-green-500 ${isActive ? 'text-green-500' : ''}`}>
+                    <Scroll size={28} />
+                </NavLink>
+            </nav>
+        </header>
     )
 }
--- src/layouts/defaults.tsx
+++ src/layouts/defaults.tsx
@@ -3,7 +3,7 @@ import { Header } from "../components/header"
 
 export function DefaultLayout() {
     return (
-        <div>
+        <div className="max-w-[74rem] h-[calc(100vh-10rem)] my-20 mx-auto p-10 bg-gray-800 rounded-lg flex flex-col">
             <Header />
             <Outlet />
         </div>
