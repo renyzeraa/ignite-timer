abafc5311107ef30bbfca451e2cc142fa69ebc16
Renan Silva <renansilvaytB@gmail.com>
2025-11-12 18:34:44 -0300

    feat: validacao de formulario

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,44 @@
-import { Play } from "phosphor-react";
+import { Play } from "phosphor-react"
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import z from "zod"
+
+const newCycleFormValidatorSchema = z.object({
+    task: z.string().min(1, 'Informe a tarefa'),
+    minutesAmount: z.number().min(5, 'O ciclo precisa ser no mínimo 5 minutos.').max(60, 'O ciclo precisa ser no máximo 60 minutos.')
+})
+
+type NewCycleFormData = z.infer<typeof newCycleFormValidatorSchema>
 
 export function Home() {
+    const { register, handleSubmit, watch, reset } = useForm({
+        resolver: zodResolver(newCycleFormValidatorSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0
+        }
+    })
+
+    function handleCreateNewCycle(data: NewCycleFormData) {
+        console.log(data)
+        reset()
+    }
+
+    const isSubmitDisabled = !watch('task')
+
     return (
         <div className="flex-1 flex flex-col items-center justify-center">
-            <form action="" className="flex flex-col items-center gap-14">
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} className="flex flex-col items-center gap-14">
                 <div className="w-full flex items-center justify-center gap-2 text-gray-100 text-lg font-bold flex-wrap">
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <input type="text" list="task-suggestions" id="task" placeholder="Dê um nome ao seu projeto" className="bg-transparent h-10 border-b-2 border-solid border-b-gray-500 font-bold text-lg text-gray-100 px-2 flex-1 placeholder:text-gray-500 focus:shadow-none focus:border-b-green-500" />
+                    <input
+                        type="text"
+                        list="task-suggestions"
+                        id="task"
+                        placeholder="Dê um nome ao seu projeto"
+                        className="bg-transparent h-10 border-b-2 border-solid border-b-gray-500 font-bold text-lg text-gray-100 px-2 flex-1 placeholder:text-gray-500 focus:shadow-none focus:border-b-green-500"
+                        {...register('task')}
+                    />
 
                     <datalist id="task-suggestions">
                         <option value="Projeto 1"></option>
@@ -16,7 +48,16 @@ export function Home() {
                     </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
-                    <input type="number" min={5} max={60} step={5} id="minutesAmount" placeholder="00" className="bg-transparent h-10 border-b-2 border-solid border-b-gray-500 font-bold text-lg text-gray-100 px-2 w-16 placeholder:text-gray-500 focus:shadow-none focus:border-b-green-500" />
+                    <input
+                        type="number"
+                        min={5}
+                        max={60}
+                        step={5}
+                        id="minutesAmount"
+                        placeholder="00"
+                        className="bg-transparent h-10 border-b-2 border-solid border-b-gray-500 font-bold text-lg text-gray-100 px-2 w-16 placeholder:text-gray-500 focus:shadow-none focus:border-b-green-500"
+                        {...register('minutesAmount', { valueAsNumber: true, max: 60, min: 5 })}
+                    />
 
                     <span>minutos.</span>
                 </div>
@@ -28,7 +69,13 @@ export function Home() {
                     <span className="bg-gray-700 rounded-lg py-8 px-4">0</span>
                 </div>
 
-                <button type="submit" className="w-full border-none rounded-lg p-4 flex justify-center items-center font-bold gap-2 cursor-pointer bg-green-500 disabled:hover:bg-green-500 disabled:cursor-not-allowed disabled:opacity-70 text-gray-100 hover:bg-green-700 transition-colors"><Play size={24} /> Começar</button>
+                <button
+                    type="submit"
+                    className="w-full border-none rounded-lg p-4 flex justify-center items-center font-bold gap-2 cursor-pointer bg-green-500 disabled:hover:bg-green-500 disabled:cursor-not-allowed disabled:opacity-70 text-gray-100 hover:bg-green-700 transition-colors"
+                    disabled={isSubmitDisabled}
+                >
+                    <Play size={24} /> Começar
+                </button>
             </form>
         </div>
     )
